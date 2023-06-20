299f280154ae030df4594a9e4dec66d18b78286a
amrithaaj31 <137217305+amrithaaj31@users.noreply.github.com>
2023-06-21 00:22:49 +0530

    Added Test File

---
+++ test.py
@@ -0,0 +1,15 @@
+import pandas
+import requests
+
+import csv
+import json
+
+df = pandas.DataFrame()
+
+
+# create proj ->
+# create virtual env (give name) ->
+# activate virtual env ->
+# pip install <specific_package> ->
+# import <package>
+
